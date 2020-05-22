070b88a4e619d1873396394919199daebbbae878
Maycon Rayone <maycon.rayone@gmail.com>
2020-05-21 22:14:36 -0300

    aprendendo a lidar com contextos.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -14,7 +14,7 @@ import Button from '../../components/Button';
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { user, signIn } = useContext(AuthContext);
 
   const handleSubmit = useCallback(
     async (data: UserCredentials): Promise<void> => {
